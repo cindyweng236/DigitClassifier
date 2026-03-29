b0d02c1ed676a4d33c78e4007fa4893fa8563de1
cindyweng672 <cindyweng236@gmail.com>
2026-03-29 17:11:15 -0400

    add cnn

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.keras import layers, models
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -6,15 +7,33 @@ mnist = tf.keras.datasets.mnist
 x_train = tf.keras.utils.normalize(x_train, axis = 1)
 x_test = tf.keras.utils.normalize(x_test, axis = 1)
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-model.add(tf.keras.layers.Dense(128, activation = 'relu'))
-model.add(tf.keras.layers.Dense(128, activation = 'relu'))
-model.add(tf.keras.layers.Dense(10, activation = 'softmax'))
+model = tf.keras.models.Sequential([
+    layers.Reshape((28, 28, 1), input_shape=(28, 28)),
+    
+    layers.Conv2D(32, (3, 3), activation = 'relu'),
+    layers.MaxPooling2D((2, 2)),
+    
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.3),
+    
+    layers.Dense(10, activation='softmax')
+])
 
-model.compile(optimizer='adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
 
-model.fit(x_train, y_train, epochs = 3)
+history = model.fit(
+    x_train, y_train,
+    epochs=5,
+    validation_data=(x_test, y_test)
+)
 
 model.save('digit.h5')
 
